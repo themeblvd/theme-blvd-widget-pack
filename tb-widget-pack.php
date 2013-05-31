6c22cd8223767caacc4104968f7eb85a21416644
Jason Bobich <jasonbobich@gmail.com>
2013-05-31 13:35:13 -0800

    Fixed wording in warning when Theme Blvd framework isn't installed, and added "Dismiss" link.

--- tb-widget-pack.php
+++ tb-widget-pack.php
@@ -41,6 +41,7 @@ function themeblvd_widget_pack_init() {
 	// Check to make sure Theme Blvd Framework 2.2+ is running
 	if( ! defined( 'TB_FRAMEWORK_VERSION' ) || version_compare( TB_FRAMEWORK_VERSION, '2.2.0', '<' ) ) {
 		add_action( 'admin_notices', 'themeblvd_widget_pack_warning' );
+		add_action( 'admin_init', 'themeblvd_widget_pack_disable_nag' );
 		return;
 	}
 	
@@ -83,7 +84,24 @@ add_action( 'plugins_loaded', 'themeblvd_widget_pack_textdomain' );
  */
 
 function themeblvd_widget_pack_warning() {
-	echo '<div class="updated">';
-	echo '<p>'.__( 'You currently have the "Theme Blvd Widget Areas" plugin activated, however you are not using a theme with Theme Blvd Framework v2.2+, and so this plugin will not do anything.', 'themeblvd_widget_pack' ).'</p>';
-	echo '</div>';
+	global $current_user;
+	// DEBUG: delete_user_meta( $current_user->ID, 'tb_widget_pack_no_framework' )
+	if( ! get_user_meta( $current_user->ID, 'tb_widget_pack_no_framework' ) ){
+		echo '<div class="updated">';
+		echo '<p>'.__( 'You currently have the "Theme Blvd Widget Pack" plugin activated, however you are not using a theme with Theme Blvd Framework v2.2+, and so this plugin will not do anything.', 'themeblvd_widget_pack' ).'</p>';
+		echo '<p><a href="?tb_nag_ignore=tb_widget_pack_no_framework">'.__('Dismiss this notice', 'themeblvd_shortcodes').'</a> | <a href="http://www.themeblvd.com" target="_blank">'.__('Visit ThemeBlvd.com', 'themeblvd_widget_pack').'</a></p>';
+		echo '</div>';
+	}
+}
+
+/**
+ * Dismiss an admin notice.
+ *
+ * @since 1.0.2
+ */
+
+function themeblvd_widget_pack_disable_nag() {
+	global $current_user;
+    if ( isset( $_GET['tb_nag_ignore'] ) )
+         add_user_meta( $current_user->ID, $_GET['tb_nag_ignore'], 'true', true );
 }
